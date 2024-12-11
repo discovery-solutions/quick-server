ff09ca4a036b55ce86ccb979728eab98802c3742
Lucas Craveiro Paes <lucascraveiropaes@gmail.com>
2024-12-11 15:46:57 -0300

    Refactor index.ts and types.ts files to export EntityManager and Database classes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,9 @@
 import { Middleware } from './servers';
+import { EntityManager } from './features/entity';
+import { Database } from './features/databases';
 import { Config } from './types';
 export * from './types';
+export { Database, EntityManager };
 export declare class QuickServer {
     config: Config;
     private middlewares;
